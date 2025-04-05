ff5ceb04639fd970433e16aafd19a1f8f4c6f248
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-04-05 23:10:41 +0200

    Answering the questions: # What is the name of highest paid person (including benefits)? # What is the name of lowest paid person (including benefits)?

--- pycharm_codes/4/01-SF Salaries Exercise/main.py
+++ pycharm_codes/4/01-SF Salaries Exercise/main.py
@@ -18,6 +18,14 @@ salaries_dataframe = pd.read_csv('Salaries.csv',low_memory=False)
 # 'JOSEPH DRISCOLL' Job title
 # print(salaries_dataframe[salaries_dataframe['EmployeeName']=='JOSEPH DRISCOLL']['JobTitle'])
 
+# How much does JOSEPH DRISCOLL make (including benefits)?
+# print(salaries_dataframe[salaries_dataframe['EmployeeName']=='JOSEPH DRISCOLL'].to_string())
+# print(salaries_dataframe[salaries_dataframe['EmployeeName']=='JOSEPH DRISCOLL']['TotalPayBenefits'])
 
+# What is the name of highest paid person (including benefits)?
+# print(salaries_dataframe[salaries_dataframe['TotalPayBenefits'] == salaries_dataframe['TotalPayBenefits'].max()]['EmployeeName'])
 
+# What is the name of lowest paid person (including benefits)?
+# print(salaries_dataframe[salaries_dataframe['TotalPayBenefits'] == salaries_dataframe['TotalPayBenefits'].min()].to_string())
 
+# What was the average (mean) BasePay of all employees per year? (2011-2014) ?
