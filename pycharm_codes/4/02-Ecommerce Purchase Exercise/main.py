6b3115d415c1a3da845de9e8c4b94b457693424b
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-04-06 19:12:54 +0200

    answering the question: how mant people have en as their language

--- pycharm_codes/4/02-Ecommerce Purchase Exercise/main.py
+++ pycharm_codes/4/02-Ecommerce Purchase Exercise/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from pandas import value_counts
 
 # importing the file
 ecom = pd.read_csv('Ecommerce Purchases')
 
 # checking the head of the file
-print(ecom.head().to_string())
+# print(ecom.head().to_string())
+print("\n ===================================================================== \n")
 
 # how many rows and columns
 # print(ecom.info())
@@ -17,4 +19,15 @@ print(ecom.head().to_string())
 # print(ecom['Purchase Price'].min())
 
 # how many people have en as their language?
+# important: there is a difference between count() and value_counts()
+# count: gives you the number of rows for each column
+# value_count: append a new column to the original dataframe and puts the number of time the same row has been repeated
+# print(ecom[ecom['Language']=='en'].count())
+
+
+
+
+
+
+
 
