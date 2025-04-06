e84136fd8279f8e18c6539e7be5d9c8ce9140ad2
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-04-06 18:53:11 +0200

    answering initial questions

--- pycharm_codes/4/02-Ecommerce Purchase Exercise/main.py
+++ pycharm_codes/4/02-Ecommerce Purchase Exercise/main.py
@@ -1,1 +1,20 @@
+import pandas as pd
+
+# importing the file
+ecom = pd.read_csv('Ecommerce Purchases')
+
+# checking the head of the file
+print(ecom.head().to_string())
+
+# how many rows and columns
+# print(ecom.info())
+
+# average purchase price
+# print(ecom['Purchase Price'].mean())
+
+# highest and lowest purchase price
+# print(ecom['Purchase Price'].max())
+# print(ecom['Purchase Price'].min())
+
+# how many people have en as their language?
 
